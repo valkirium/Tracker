80149c3bb30386e8e6a52791c49ae3b56084cbe9
valkirium <maximumnoff@gmail.com>
2022-02-04 21:55:05 +0400

    add checks

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Tracker.go
 go 1.17
 
 require (
+	golang.org/x/build v0.0.0-20220201010333-015a2a0c58f3
 	k8s.io/apimachinery v0.23.0
 	k8s.io/client-go v0.23.0
 )
@@ -12,7 +13,7 @@ require (
 	github.com/go-logr/logr v1.2.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/imdario/mergo v0.3.5 // indirect
@@ -20,7 +21,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
--- tracker.go
+++ tracker.go
@@ -12,15 +12,21 @@ import (
 //	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+//	"golang.org/x/build/kubernetes/api"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// This program lists the pods in a cluster equivalent to
+// This program lists the pods in a cluster equivalent toда
 //
 // kubectl get pods
 //
 func main() {
 	var ns string
+	var i int = 0
+	var a int = 0
+	var b int = 0
+	var c int = 0
+	var d int = 0
 	flag.StringVar(&ns, "namespace", "", "namespace")
 
 	// Bootstrap k8s configuration from local       Kubernetes config file
@@ -41,50 +47,96 @@ func main() {
 	if err != nil {
 		log.Fatalln("failed to get nodes:", err)
 	}
-	for _, node := range nodes.Items {
-		fmt.Printf("%s\n", node.Name)
+	for index, node := range nodes.Items {
+		fmt.Printf("%d\t %s\n", index, node.Name)
 		for _, condition := range node.Status.Conditions {
 			fmt.Printf("\t%s: %s\n", condition.Type, condition.Status)
+			if condition.Type == "NetworkUnavailable" && condition.Status == "False" {
+		fmt.Println("Ok")
+		i++
+			}
+			if condition.Type == "MemoryPressure" && condition.Status == "False" {
+                fmt.Println("Ok")
+		i++
+                        }
+			if condition.Type == "DiskPressure" && condition.Status == "False" {
+                fmt.Println("Ok")
+		i++
+                        }
+			if condition.Type == "PIDPressure" && condition.Status == "False" {
+                fmt.Println("Ok")
+		i++
+                        }
+			if condition.Type == "Ready" && condition.Status == "True" {
+                fmt.Println("Ok")
+		i++
+                        }
+		a = (index + 1) * 5
 		}
 	}
+
 	pods, err := clientset.CoreV1().Pods("kube-system").List(context.TODO(), metav1.ListOptions{})
         if err != nil {
                 log.Fatalln("failed to get pods:", err)
         }
-        for _, pod := range pods.Items {
-                fmt.Printf("%s\n", pod.Name)
+        for index, pod := range pods.Items {
+                fmt.Printf("%d\t %s\n", index,  pod.Name)
                 for _, condition := range pod.Status.Conditions {
                         fmt.Printf("\t%s: %s\n", condition.Type, condition.Status)
+			if condition.Type == "Initialized" && condition.Status == "True" {
+                fmt.Println("Ok")
+                i++
+                        }
+			if condition.Type == "Ready" && condition.Status == "True" {
+                fmt.Println("Ok")
+                i++
+                        }
+			if condition.Type == "ContainersReady" && condition.Status == "True" {
+                fmt.Println("Ok")
+                i++
+                        }
+			 if condition.Type == "PodScheduled" && condition.Status == "True" {
+                fmt.Println("Ok")
+                i++
+                        }
+		b = (index + 1) * 4
                 }
 	}
 	Deployments, err := clientset.AppsV1().Deployments("kube-system").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		log.Fatalln("failed to get deployments:", err)
         }
-	for _, Deployment := range Deployments.Items {
-		fmt.Printf("%s\n", Deployment.Name)
+	for index, Deployment := range Deployments.Items {
+		fmt.Printf("%d\t %s\n", index, Deployment.Name)
 		fmt.Print("Ready: ")
 		fmt.Printf("%d\n", Deployment.Status.ReadyReplicas)
-		fmt.Print("Up to date: ") 
-		fmt.Printf("%d\n", Deployment.Status.UpdatedReplicas) 
+		fmt.Print("Up to date: ")
+		fmt.Printf("%d\n", Deployment.Status.UpdatedReplicas)
 		if Deployment.Status.ReadyReplicas == Deployment.Status.UpdatedReplicas {
 		fmt.Println("Ok")
+		i++
 		}
+		c = index + 1
 	}
 	DaemonSets, err := clientset.AppsV1().DaemonSets("kube-system").List(context.TODO(), metav1.ListOptions{})
         if err != nil {
                 log.Fatalln("failed to get daemonset:", err)
         }
-	for _, DaemonSet := range DaemonSets.Items {
-                fmt.Printf("%s\n", DaemonSet.Name)
+	for index, DaemonSet := range DaemonSets.Items {
+                fmt.Printf("%d\t %s\n", index, DaemonSet.Name)
 		fmt.Print("Ready: ")
 		fmt.Printf("%d\n", DaemonSet.Status.NumberReady)
 		fmt.Print("Up to date: ")
 		fmt.Printf("%d\n", DaemonSet.Status.UpdatedNumberScheduled)
 		if  DaemonSet.Status.NumberReady == DaemonSet.Status.UpdatedNumberScheduled {
 		fmt.Println("Ok")
+		i++
 		}
+		d = index + 1
 	}
-
+        if i == (a + b + c + d) {
+		fmt.Println("OK")
+	}else{fmt.Println("Cluster is failed")
+}
 }
 
