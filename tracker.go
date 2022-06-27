4b2dffbafd5bfcf7fbe1e641ff84a6a144cb65e4
Maksim Umnov <maksim.umnov@netcracker.com>
2022-06-27 13:56:32 +0400

    delete fmt.Println("ok")

--- tracker.go
+++ tracker.go
@@ -297,11 +297,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Create an rest client not targeting specific API version
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var cloudup = normstat("Ok", "fail")
 	fmt.Println(cloudup)
 }
